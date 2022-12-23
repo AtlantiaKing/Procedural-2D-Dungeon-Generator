46d002b50c5de8ea8dcff1a98722c68a9993b683
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2022-12-23 11:10:18 +0100

    Added Vector2 Operators

--- Project/DataTypes.h
+++ Project/DataTypes.h
@@ -6,6 +6,15 @@ struct Vector2
 {
 	int x{};
 	int y{};
+
+	Vector2 operator+(const Vector2& other) const
+	{ 
+		return Vector2{ x + other.x, y + other.y }; 
+	}
+	Vector2 operator-(const Vector2& other) const
+	{
+		return Vector2{ x - other.x, y - other.y }; 
+	}
 };
 
 struct Color
